460d26bfeac6c010cb731e2f702abd90f698b50c
Angel Garcia <4ng4rp3@gmail.com>
2019-05-13 10:18:34 +0200

    Clase jugador y modificaciones esteticas

--- src/game/player.js
+++ src/game/player.js
@@ -2,7 +2,7 @@ class player {
     constructor(matter, playerNumber, shapes) {
         this.playerNumber = playerNumber;
         this.gameMatter = matter;
-        this.velocidad = 10;
+        this.velocidad = 5;
         this.fuerza = 10;
         this.nombre = "Dummy Name";
         this.fragmentos = 0;
@@ -11,11 +11,30 @@ class player {
         this.imanes = 0;
 
         this.ficha = this.gameMatter.add.sprite(600, 200, 'sheet', 'ficha2.png', {shape: shapes.ficha2});
+        this.ficha.setFixedRotation();
+        //this.ficha.
 
         //fragmentos, teleportadores, artefactos, edulcorantes, imanes magicos,
         //fuerza, velocidad, nombre
     }
-    
+
+    moverDerecha(boolean){
+        if (boolean) this.ficha.setVelocityX(this.velocidad);
+        //else this.ficha.setVelocityX(0);
+    }
+    moverIzquierda(boolean){
+        if (boolean) this.ficha.setVelocityX(- this.velocidad);
+        else this.ficha.setVelocityX(0);
+    }
+    moverArriba(boolean){
+        if (boolean) this.ficha.setVelocityY(- this.velocidad);
+        else this.ficha.setVelocityY(0);
+    }
+    moverAbajo(boolean){
+        if (boolean) this.ficha.setVelocityY(this.velocidad);
+        //else this.ficha.setVelocityY(0);
+    }
+
     //GETTERS
     getNombre()
     {
--- src/game/scenes/PlayScene.js
+++ src/game/scenes/PlayScene.js
@@ -2,6 +2,11 @@ import { Scene } from 'phaser';
 import { player } from '../player'
 import ee from '@/ee'
 
+//VARIABLES
+var player1;
+var player2;
+var cursors;
+var wasd;
 
 export default class PlayScene extends Scene {
   constructor () {
@@ -12,9 +17,11 @@ export default class PlayScene extends Scene {
     console.log("Starting PlayScene ...");
     var shapes = this.cache.json.get('shapes');
     this.matter.world.setBounds(0, 0, this.game.config.width, this.game.config.height);
+    //this.game.physics.gravity.y = 0;
+    this.matter.world.disableGravity();
 
     //AÑADIR MAPA
-    var ground = this.matter.add.sprite(600, 220, 'sheet', 'mapa.png', {shape: shapes.mapa});
+    var ground = this.matter.add.sprite(600, 240, 'sheet', 'mapa.png', {shape: shapes.mapa});
     //ground.setPosition(0 + ground.centerOfMass.x, 280 + ground.centerOfMass.y);  // position (0,280)
     //ground.setPosition(600, 220);  // position (0,280)
     ground.setScale(1.2);
@@ -25,17 +32,20 @@ export default class PlayScene extends Scene {
     background.setDepth(-1);
 
     //AÑADIR FICHA DEL JUGADOR 1
-    var player1 = new player(this.matter, 1, shapes);
+    player1 = new player(this.matter, 1, shapes);
 
     //AÑADIR FICHA DEL JUGADOR 1
-    var player2 = new player(this.matter, 2, shapes);
+    player2 = new player(this.matter, 2, shapes);
 
-    //CONSEGUIR LA INSTANCIA DEL TECLADO
-    var cursors = this.input.keyboard.createCursorKeys();
+    //CONSEGUIR LA INSTANCIAS DEL TECLADO
+    cursors = this.input.keyboard.createCursorKeys();
 
-
-    //console.log(player1.getFragmentos());
-    //var ficha = this.matter.add.sprite(600, 200, 'sheet', 'ficha2.png', {shape: shapes.ficha2});
+    wasd = this.input.keyboard.addKeys({
+        'up': Phaser.Input.Keyboard.KeyCodes.W,
+        'down': Phaser.Input.Keyboard.KeyCodes.S,
+        'left': Phaser.Input.Keyboard.KeyCodes.A,
+        'right': Phaser.Input.Keyboard.KeyCodes.D
+    });
 
     /*const bomb = this.physics.add.image(400, 200, 'bomb');
     bomb.setCollideWorldBounds(true);
@@ -50,16 +60,101 @@ export default class PlayScene extends Scene {
   }
 
   update () {
-    //Comprobar las si el jugador 1 ha pulsado alguna de sus teclas
-    this.inputsJug1();
+      //Comprobar si el jugador 1 ha pulsado alguna de sus teclas
+      this.inputsJug1(player1);
 
-    //Comprobar las si el jugador 1 ha pulsado alguna de sus teclas
-    this.inputsJug1();
+      //Comprobar si el jugador 2 ha pulsado alguna de sus teclas
+      this.inputsJug2(player2);
+
+      //Derrumbar nodos
+      this.derrumbeNodos();
+
+      //Derrumbe total
+      this.derrumbeTotal();
   }
-  inputsJug1() {
+  spawnCofres(){
 
   }
-  inputsJug2() {
+  spawnFragmentos(){
 
   }
+  derrumbeNodos(){
+      //Funcion que ira derrumbando nodos aleatoriamente
+  }
+
+  derrumbeTotal(){
+      //A partir de 1 minuto el juego puede acabarse repentinamente por un derrumbe total
+
+  }
+
+  inputsJug1(jugador) {
+      let input = false;
+      //Movimiento a la izquierda
+      if(cursors.left.isDown){
+          jugador.moverIzquierda(true);
+      }
+      else {
+          jugador.moverIzquierda(false);
+      }
+
+      //Movimiento a la derecha
+      if(cursors.right.isDown){
+          jugador.moverDerecha(true);
+      }
+      else {
+          jugador.moverDerecha(false);
+      }
+
+      //Movimiento a arriba
+      if(cursors.up.isDown){
+          jugador.moverArriba(true);
+      }
+      else {
+          jugador.moverArriba(false);
+      }
+
+      //Movimiento a abajo
+      if(cursors.down.isDown){
+          jugador.moverAbajo(true);
+      }
+      else {
+          jugador.moverAbajo(false);
+      }
+
+  }
+    inputsJug2(jugador) {
+        let input = false;
+        //Movimiento a la izquierda
+        if(wasd.left.isDown){
+            jugador.moverIzquierda(true);
+        }
+        else {
+            jugador.moverIzquierda(false);
+        }
+
+        //Movimiento a la derecha
+        if(wasd.right.isDown){
+            jugador.moverDerecha(true);
+        }
+        else {
+            jugador.moverDerecha(false);
+        }
+
+        //Movimiento a arriba
+        if(wasd.up.isDown){
+            jugador.moverArriba(true);
+        }
+        else {
+            jugador.moverArriba(false);
+        }
+
+        //Movimiento a abajo
+        if(wasd.down.isDown){
+            jugador.moverAbajo(true);
+        }
+        else {
+            jugador.moverAbajo(false);
+        }
+
+    }
 }
